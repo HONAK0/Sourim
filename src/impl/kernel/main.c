a1ebf50a0139d614654c3f2dfe3f3fb8aa934bc0
HONAK <88250329+HONAK0@users.noreply.github.com>
2022-07-11 14:58:08 +0000

    Add files via upload

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,33 +1,28 @@
 #include "terminal.h"
 #include "drivers/keyboard.h"
 #include "str.h"
+#include "drivers/port.h"
 #include "user_files/pcmem.h"
-
-#define NAMEOS "Sourim"
-#define VERSIONOS "1.0"
+#include "contol/power.h"
 
 void kstart(){
-    print("Welcome!\nOS: ");
-    print(NAMEOS);
-    print("\nVersion Kernel: ");
-    print(VERSIONOS);
-    print("\n\n");
+    tset_color(LIGHT_CYAN,BLACK);
+    print("Welcome to Sourim OS\n\n");
+    tset_color(WHITE,BLACK);
 }
 
-void print_log(enum color log_color, char *log, char *message){
-    print("[");
+void print_log(enum color log_color, char *log, char *message, char *joiner){
     tset_color(log_color, BLACK);
     print(log);
     tset_color(WHITE, BLACK);
-    print("] ");
+    print(joiner);
     print(message);
 }
 
 void krun(){
     char enter_input[512];
-    tset_color(GREEN, BLACK);
-    print(namepc);
     print("@");
+    tset_color(CYAN, BLACK);
     print(username);
     tset_color(WHITE, BLACK);
     print(" $ ");
@@ -46,13 +41,17 @@ void krun(){
             for(int i = strlen(command)+1; i < strlen(enter_input); i++){            
                 tputchar(enter_input[i]);
             }
+            tputchar('\n');
         } else {
-            print_log(RED, "ERROR", "Arguments is empty\n");
+            print_log(RED, "ERROR", "Arguments is empty\n", " > ");
         }
     } else if(strcheck(command, "help", 32)){
-        print("echo <text*> - Print the text in terminal\n");
-        print("clear - Clear screen\n");
-        print("setusername <name*> - Set username\n");
+        print_log(LIGHT_GREEN, "echo <text*>","        Print the text in terminal\n","");
+        print_log(LIGHT_GREEN, "clear","               Clear screen\n",              "");
+        print_log(LIGHT_GREEN, "setusername <name*>"," Set username\n",              "");
+        print_log(LIGHT_GREEN, "russia","              Show Russian flag\n",         "");
+        print_log(LIGHT_GREEN, "shutdown","            Power off\n",                 "");
+        print_log(LIGHT_GREEN, "reboot","              Restart machine\n",           "");
     } else if(strcheck(command, "clear", 32)){
         clear();
     } else if(strcheck(command, "setusername", 32)){
@@ -62,11 +61,29 @@ void krun(){
                 username[ii] = enter_input[i];
                 ii++;
             }
-            print_log(GREEN, "OK", "Username is changed\n");
+            print_log(GREEN, "OK", "Username is changed\n", " > ");
         } else {
-            print_log(RED, "ERROR", "Arguments is empty\n");
+            print_log(RED, "ERROR", "Arguments is empty\n", " > ");
         }
+    } else if(strcheck(command, "russia", 32)){
+        for(int y = 0; y < 3; y++){
+            for(int x = 0; x < 10; x++){
+                if(y == 0){
+                    tset_color(WHITE, WHITE);
+                } else if(y == 1){
+                    tset_color(WHITE, BLUE);
+                } else if(y == 2){
+                    tset_color(WHITE, RED);
+                }
+                tputchar(' ');
+            }
+            tputchar('\n');
+        }
+    } else if(strcheck(command, "reboot", 32)){
+        reboot();
+    } else if(strcheck(command, "shutdown", 32)){
+        shutdown();
     } else {
-        print_log(CYAN, "WARN", "Unknown command, please type 'help'\n");
+        print_log(CYAN, "WARN", "Unknown command, please type 'help'\n", " > ");
     }
 }
---
+++ src/impl/x86_64/contol/power.c
@@ -0,0 +1,30 @@
+#include "drivers/port.h"
+#include "terminal.h"
+
+#define bit(n) (1<<(n))
+#define check_flag(flags, n) ((flags) & bit(n))
+
+void reboot(){
+    clear();
+    print("Rebooting...");
+    uint8_t v;
+    asm volatile ("cli");
+    do
+    {
+        v = port_byte_in(0x64);
+        if (check_flag(v, 0) != 0)
+            port_byte_in(0x60);
+    } while (check_flag(v, 1) != 0);
+ 
+    port_byte_out(0x64, 0xfe);
+loop:
+    asm volatile ("hlt");
+    goto loop;
+}
+void shutdown(){
+    clear();
+    print("Shutdowing...");
+    port_word_out(0xB004, 0x2000);
+    port_word_out(0x604, 0x2000);
+    port_word_out(0x4004, 0x3400);
+}
